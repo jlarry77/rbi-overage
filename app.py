1214e42ebb2afec47576149d9e25c6dc560e8ef6
Justyn <110492404+jlarry77@users.noreply.github.com>
2024-10-01 20:10:47 +0000

    Add a bit of .css and python to app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',
+                          #data_table=DATA_TABLE,
+                          company_name="Roof Brokers, Inc.")
 
 if __name__ == '__main__':
   app.run('0.0.0.0', debug=True) 
